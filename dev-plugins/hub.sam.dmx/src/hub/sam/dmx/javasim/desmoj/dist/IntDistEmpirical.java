c785522281078cdc4b15c107d336538ca0976b72
Andreas Blunk <andreas.blunk@gmail.com>
2013-04-18 14:38:03 +0200

    bugfix in BL-to-JiST transformation. added external functions example.

---
+++ dev-plugins/hub.sam.dmx/src/hub/sam/dmx/javasim/desmoj/dist/IntDistEmpirical.java
@@ -0,0 +1,11 @@
+package hub.sam.dmx.javasim.desmoj.dist;
+
+import hub.sam.dmx.javasim.desmoj.DefaultSimulation;
+
+public class IntDistEmpirical extends desmoj.core.dist.IntDistEmpirical {
+	
+	public IntDistEmpirical(String name) {
+		super(DefaultSimulation.DEFAULT, name, DefaultSimulation.DEFAULT.showInReport, DefaultSimulation.DEFAULT.showIntrace);		
+	}
+
+}
