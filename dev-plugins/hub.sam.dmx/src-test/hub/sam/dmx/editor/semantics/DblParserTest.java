441c81adb6a23c354486f1458f72545bb7f14279
Andreas Blunk <andreas.blunk@maibornwolff.de>
2017-04-04 00:14:54 +0200

    parametrized parser tests

--- dev-plugins/hub.sam.dmx/src-test/hub/sam/dmx/editor/semantics/DblParserTest.java
+++ dev-plugins/hub.sam.dmx/src-test/hub/sam/dmx/editor/semantics/DblParserTest.java
@@ -5,13 +5,20 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.apache.bsf.util.IOUtils;
 import org.apache.commons.io.FileUtils;
 import org.assertj.core.api.Assertions;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import hub.sam.dmx.TestDataDriver;
 import hub.sam.dmx.editor.modelcreation.DblParser;
@@ -19,20 +26,32 @@ import hub.sam.tef.modelcreating.IModelCreatingContext;
 import hub.sam.tef.modelcreating.ModelCreatingException;
 import hub.sam.tef.modelcreating.ParserError;
 
+@RunWith(Parameterized.class)
 public class DblParserTest {
 	
-	private TestDataDriver testDataDriver = new TestDataDriver();
+	private static TestDataDriver testDataDriver;
 
+	@Parameters
+	public static Object[] testFiles() {
+	    return new Object[] { "hello.dbl", "yieldto.dbl" };
+	}
+	
+	@Parameter
+	public String fileName;
+	
+	@BeforeClass
+	public static void setup() {
+		testDataDriver = new TestDataDriver();
+		testDataDriver.setupEnvironment();		
+	}
+    
 	@Test
-	public void helloWorld() throws ModelCreatingException, IOException {
-		testDataDriver.setupEnvironment();
-		
+	public void parseEachTestFile() throws ModelCreatingException, IOException {
 		IPath inputPath = new Path(testDataDriver.getDmxPluginLocation());		
-		DblParser dblParser = new DblParser(inputPath, "hello.dbl");
+		DblParser dblParser = new DblParser(inputPath, fileName);
 		dblParser.setPluginFileLocator(testDataDriver.getMockedDblPluginFileLocator());
 		
-		// TODO test all files with parametric test
-		java.nio.file.Path blFile = Paths.get(URI.create(testDataDriver.getDmxPluginLocation() + "/resources/test/hello.dbl"));
+		java.nio.file.Path blFile = Paths.get(URI.create(testDataDriver.getDmxPluginLocation() + "/resources/test/" + fileName));
 		String fileContent = FileUtils.readFileToString(blFile.toFile(), Charset.forName("utf-8"));
 		IModelCreatingContext modelContext = dblParser.parse(fileContent);
 		
