d374f19376d1014d391ab7a873aeb046d4bcce5f
Andreas Blunk <andreas.blunk@maibornwolff.de>
2017-04-03 23:58:36 +0200

    improved tests

--- dev-plugins/hub.sam.dmx/src-test/hub/sam/dmx/editor/semantics/DblParserTest.java
+++ dev-plugins/hub.sam.dmx/src-test/hub/sam/dmx/editor/semantics/DblParserTest.java
@@ -1,7 +1,13 @@
 package hub.sam.dmx.editor.semantics;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Paths;
 
+import org.apache.bsf.util.IOUtils;
+import org.apache.commons.io.FileUtils;
 import org.assertj.core.api.Assertions;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
@@ -11,21 +17,38 @@ import hub.sam.dmx.TestDataDriver;
 import hub.sam.dmx.editor.modelcreation.DblParser;
 import hub.sam.tef.modelcreating.IModelCreatingContext;
 import hub.sam.tef.modelcreating.ModelCreatingException;
+import hub.sam.tef.modelcreating.ParserError;
 
 public class DblParserTest {
 	
 	private TestDataDriver testDataDriver = new TestDataDriver();
 
 	@Test
-	public void helloWorld() throws ModelCreatingException, MalformedURLException {
+	public void helloWorld() throws ModelCreatingException, IOException {
 		testDataDriver.setupEnvironment();
 		
 		IPath inputPath = new Path(testDataDriver.getDmxPluginLocation());		
-		DblParser dblParser = new DblParser(inputPath, "hello-world.dbl");
+		DblParser dblParser = new DblParser(inputPath, "hello.dbl");
 		dblParser.setPluginFileLocator(testDataDriver.getMockedDblPluginFileLocator());
-		IModelCreatingContext context = dblParser.parse("module test { void main() { print \"hello\"; } }");
 		
-		Assertions.assertThat(context.getErrors()).isEmpty();
-	}
-	
+		// TODO test all files with parametric test
+		java.nio.file.Path blFile = Paths.get(URI.create(testDataDriver.getDmxPluginLocation() + "/resources/test/hello.dbl"));
+		String fileContent = FileUtils.readFileToString(blFile.toFile(), Charset.forName("utf-8"));
+		IModelCreatingContext modelContext = dblParser.parse(fileContent);
+		
+		// TODO compile DBL to target language and execute
+		
+		if (!modelContext.getErrors().isEmpty()) {
+			modelContext.getErrors().stream().forEach(error -> {
+				String message = error.getMessage();
+				if (error instanceof ParserError) {
+					ParserError parserError = (ParserError) error;
+					message += " at position: " + parserError.getPosition(null);
+				}
+				System.out.println(message);
+			});
+			Assertions.fail("found parse errors.");
+		}
+	}	
+
 }
